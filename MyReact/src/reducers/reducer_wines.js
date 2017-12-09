c1eeaf4a5a5e7ac7154fe920b8295fab277d717a
Hiroshi <usui@berkeley.edu>
2017-12-09 15:26:58 -0800

    Create the wines reducer object, which contains two wines' data so far. Plan on testing them on components soon.

---
+++ MyReact/src/reducers/reducer_wines.js
@@ -0,0 +1,16 @@
+export default function() {
+  return [
+    {
+      name: "Cabernet Sauvignon 2007",
+      profile: { sweetness: 4, acidity: 2, alcohol: 4, body: 5 },
+      aroma: ["Red Plump", "Strawberries", "Cinnamon", "Rosemary"],
+      pairing: ["Feta Cheese", "Pork Chop", "Morels & Porcini", "Olive"]
+    },
+    {
+      name: "Pinot Noir 2007",
+      profile: { sweetness: 4, acidity: 2, alcohol: 5, body: 4 },
+      aroma: ["Cherry", "Fig", "Toasted Marshmallow", "Marzipan"],
+      pairing: ["Mozzarella", "Garlic", "Salmon", "Veal"]
+    }
+  ];
+}
