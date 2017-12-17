99f3e45bfdf5e725d904285ff8dddb9f193b1045
Hiroshi <usui@berkeley.edu>
2017-12-17 00:43:29 -0800

    Add optionsList: OptionsListReducer and import statement

--- MyReact/src/reducers/index.js
+++ MyReact/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import WheelButtonsReducer from "./reducer_wheel_buttons";
 import ActiveWheelButtonReducer from "./reducer_active_wheel_button";
+import OptionsListReducer from "./reducer_options_list.js";
 
 const rootReducer = combineReducers({
   wheelbuttons: WheelButtonsReducer,
-  activeWheelButton: ActiveWheelButtonReducer
+  activeWheelButton: ActiveWheelButtonReducer,
+  optionsList: OptionsListReducer
 });
 
 export default rootReducer;
