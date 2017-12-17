64f9f45a7466604d191d9f92201a8ac85b4cd28c
Hiroshi <usui@berkeley.edu>
2017-12-17 00:42:55 -0800

    Add reducer that produces options list.

---
+++ MyReact/src/reducers/reducer_options_list.js
@@ -0,0 +1,73 @@
+export default function(state, action) {
+  return [
+    {
+      selection: "My Mood",
+      items: [
+        { text: "Smile & Celebrate", caption: "Smile & celebrate", image: "" },
+        { text: "WOW!", caption: "Enjoy the show!", image: "" },
+        { text: "Mmmh...", caption: "Relax & meditate", image: "" }
+      ]
+    },
+    {
+      selection: "Color",
+      items: [
+        { text: "Red", image: "" },
+        { text: "White", image: "" },
+        { text: "Rose", image: "" },
+        { text: "Sparkling", image: "" }
+      ]
+    },
+    {
+      selection: "Context",
+      items: [
+        { text: "Family", image: "" },
+        { text: "Friends", image: "" },
+        { text: "Business", image: "" },
+        { text: "Date", image: "" },
+        { text: "Party", image: "" },
+        { text: "Alone", image: "" }
+      ]
+    },
+    {
+      selection: "Pairing",
+      items: [
+        { text: "Red Meat", image: "" },
+        { text: "White Meat", image: "" },
+        { text: "Cheese", image: "" }
+      ]
+    },
+    {
+      selection: "Storage",
+      items: [
+        { text: "Drink Now", image: "" },
+        { text: "6 Months", image: "" },
+        { text: "1 Year", image: "" },
+        { text: "5 Years", image: "" },
+        { text: "10 Years", image: "" },
+        { text: "25 Years", image: "" }
+      ]
+    },
+    {
+      selection: "Location",
+      items: [{ text: "Europe", image: "" }, { text: "New World", image: "" }]
+    },
+    {
+      selection: "Profile",
+      items: [
+        { text: "Sweetness: Low", image: "" },
+        { text: "Sweetness: Medium", image: "" },
+        { text: "Sweetness: High", image: "" }
+      ]
+    },
+    {
+      selection: "Varietals",
+      items: [
+        { text: "Moscato", image: "" },
+        { text: "Chardonnay", image: "" },
+        { text: "Riesling", image: "" },
+        { text: "Chenin Blanc", image: "" },
+        { text: "Tremparillo", image: "" }
+      ]
+    }
+  ];
+}
