4ef19c3473dda6b26c846c561ba4a57038c98543
Hiroshi <usui@berkeley.edu>
2017-12-17 00:43:45 -0800

    Add a lot of button handling logic for option buttons.

--- MyReact/src/containers/wheel-button-detail.js
+++ MyReact/src/containers/wheel-button-detail.js
@@ -2,20 +2,51 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class WheelButtonDetail extends Component {
+  getItemsList() {
+    for (var i in this.props.optionslist) {
+      if (
+        this.props.optionslist[i].selection == this.props.activewheelbutton.name
+      ) {
+        return this.props.optionslist[i].items;
+      }
+    }
+    return;
+  }
+
+  renderOptions() {
+    if (this.getItemsList()) {
+      return this.getItemsList().map(item => {
+        return (
+          <li key={item.text}>
+            <button
+              key={item.text}
+              type="button"
+              className="btn btn-primary"
+              data-toggle="button"
+              aria-pressed="false"
+              autoComplete="off"
+            >
+              {item.text}
+            </button>
+          </li>
+        );
+      });
+    }
+  }
   render() {
     if (!this.props.activewheelbutton) {
       return <div>Please select a wheel button.</div>;
     }
     return (
       <div
-        className="center-align"
         style={{
           height: "600px",
           width: "200px",
           backgroundColor: "white"
         }}
       >
-        {this.props.activewheelbutton.name}
+        <div>{this.props.activewheelbutton.name}</div>
+        <ul className="list-group col-sm-1">{this.renderOptions()}</ul>
       </div>
     );
   }
@@ -23,7 +54,8 @@ class WheelButtonDetail extends Component {
 
 function mapStateToProps(state) {
   return {
-    activewheelbutton: state.activeWheelButton
+    activewheelbutton: state.activeWheelButton,
+    optionslist: state.optionsList
   };
 }
 
