ae2623e858e2185151d65f75c1be38e86a521b28
Hiroshi <usui@berkeley.edu>
2017-12-16 22:42:22 -0800

    Rename ActiveWheelButton import

--- MyReact/src/reducers/index.js
+++ MyReact/src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 
 import WheelButtonsReducer from "./reducer_wheel_buttons";
-import ActiveWheelButton from "./reducer_active_wheel_button";
+import ActiveWheelButtonReducer from "./reducer_active_wheel_button";
 
 const rootReducer = combineReducers({
   wheelbuttons: WheelButtonsReducer,
-  activeWheelButton: ActiveWheelButton
+  activeWheelButton: ActiveWheelButtonReducer
 });
 
 export default rootReducer;
