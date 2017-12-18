271e10330fde1cc8ba9129eb388a079071fbf31c
Kavin Tjhan <>
2017-12-17 19:40:55 -0800

    minor changes

--- MyReact/src/reducers/reducer_options_list.js
+++ MyReact/src/reducers/reducer_options_list.js
@@ -3,70 +3,73 @@ export default function(state, action) {
     {
       selection: "My Mood",
       items: [
-        { text: "Yummy", caption: "Smile & celebrate", image: "" },
-        { text: "WOW!", caption: "Enjoy the show!", image: "" },
-        { text: "Mmmh...", caption: "Relax & meditate", image: "" }
+        { text: "Yummy", selected: 0, caption: "Smile & celebrate", image: "" },
+        { text: "WOW!", selected: 0, caption: "Enjoy the show!", image: "" },
+        { text: "Mmmh...", selected: 0, caption: "Relax & meditate", image: "" }
       ]
     },
     {
       selection: "Color",
       items: [
-        { text: "Red", image: "" },
-        { text: "White", image: "" },
-        { text: "Rose", image: "" },
-        { text: "Sparkling", image: "" }
+        { text: "Red", selected: 0, image: "" },
+        { text: "White", selected: 0, image: "" },
+        { text: "Rose", selected: 0, image: "" },
+        { text: "Sparkling", selected: 0, image: "" }
       ]
     },
     {
       selection: "Context",
       items: [
-        { text: "Family", image: "" },
-        { text: "Friends", image: "" },
-        { text: "Business", image: "" },
-        { text: "Date", image: "" },
-        { text: "Party", image: "" },
-        { text: "Alone", image: "" }
+        { text: "Family", selected: 0, image: "" },
+        { text: "Friends", selected: 0, image: "" },
+        { text: "Business", selected: 0, image: "" },
+        { text: "Date", selected: 0, image: "" },
+        { text: "Party", selected: 0, image: "" },
+        { text: "Alone", selected: 0, image: "" }
       ]
     },
     {
       selection: "Pairing",
       items: [
-        { text: "Red Meat", image: "" },
-        { text: "White Meat", image: "" },
-        { text: "Cheese", image: "" }
+        { text: "Red Meat", selected: 0, image: "" },
+        { text: "White Meat", selected: 0, image: "" },
+        { text: "Cheese", selected: 0, image: "" }
       ]
     },
     {
       selection: "Storage",
       items: [
-        { text: "Drink Now", image: "" },
-        { text: "6 Months", image: "" },
-        { text: "1 Year", image: "" },
-        { text: "5 Years", image: "" },
-        { text: "10 Years", image: "" },
-        { text: "25 Years", image: "" }
+        { text: "Drink Now", selected: 0, image: "" },
+        { text: "6 Months", selected: 0, image: "" },
+        { text: "1 Year", selected: 0, image: "" },
+        { text: "5 Years", selected: 0, image: "" },
+        { text: "10 Years", selected: 0, image: "" },
+        { text: "25 Years", selected: 0, image: "" }
       ]
     },
     {
       selection: "Location",
-      items: [{ text: "Europe", image: "" }, { text: "New World", image: "" }]
+      items: [
+        { text: "Europe", selected: 0, image: "" },
+        { text: "New World", selected: 0, image: "" }
+      ]
     },
     {
       selection: "Profile",
       items: [
-        { text: "Sweetness: Low", image: "" },
-        { text: "Sweetness: Medium", image: "" },
-        { text: "Sweetness: High", image: "" }
+        { text: "Sweetness: Low", selected: 0, image: "" },
+        { text: "Sweetness: Medium", selected: 0, image: "" },
+        { text: "Sweetness: High", selected: 0, image: "" }
       ]
     },
     {
       selection: "Varietals",
       items: [
-        { text: "Moscato", image: "" },
-        { text: "Chardonnay", image: "" },
-        { text: "Riesling", image: "" },
-        { text: "Chenin Blanc", image: "" },
-        { text: "Tremparillo", image: "" }
+        { text: "Moscato", selected: 0, image: "" },
+        { text: "Chardonnay", selected: 0, image: "" },
+        { text: "Riesling", selected: 0, image: "" },
+        { text: "Chenin Blanc", selected: 0, image: "" },
+        { text: "Tremparillo", selected: 0, image: "" }
       ]
     }
   ];
