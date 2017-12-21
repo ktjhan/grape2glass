50981d9503414e4237d7e4de8c3acf4d1228f841
Hiroshi <usui@berkeley.edu>
2017-12-20 16:51:42 -0800

    Google OAuth 2.0 initiates. Need to finish. Opens mLab connection.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./services/passport.js");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
-  })
-);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
